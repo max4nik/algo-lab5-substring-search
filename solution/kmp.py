525b92e649db15fa2ee5de4717f0bc04c2cb1c9d
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-12-15 22:49:14 +0200

    added implementation of finding multiple matches

--- solution/kmp.py
+++ solution/kmp.py
@@ -3,23 +3,24 @@ def kmp_search(string, substring):
     search substring in string using longest proper prefix list
     :param string: string to find in
     :param substring: substring to find in string
-    :return: True if finds, else False
+    :return: list of tuples with index of start position of substring and last if finds, else empty list
     """
     lps = form_lps(substring)
+    result = []
     string_iterator = 0
     substring_iterator = 0
     while string_iterator < len(string):
         if substring[substring_iterator] == string[string_iterator]:
             string_iterator += 1
             substring_iterator += 1
-        if substring_iterator == len(substring):
-            return True
-        elif string_iterator < len(string) and substring[substring_iterator] != string[string_iterator]:
-            if substring_iterator != 0:
+            if substring_iterator == len(substring):
+                result.append((string_iterator - substring_iterator, string_iterator - 1))
                 substring_iterator = lps[substring_iterator - 1]
-            else:
-                string_iterator += 1
-    return False
+        elif substring_iterator != 0:
+            substring_iterator = lps[substring_iterator - 1]
+        else:
+            string_iterator += 1
+    return result
 
 
 def form_lps(substring):
@@ -36,7 +37,7 @@ def form_lps(substring):
             match_index += 1
             lps[iterator] = match_index
             iterator += 1
-        elif substring[iterator] != substring[match_index] and match_index != 0:
+        elif match_index != 0:
             match_index = lps[match_index - 1]
         else:
             lps[iterator] = 0
