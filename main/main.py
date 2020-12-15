4584537748ecb928f2936189d1f47a9c917feb15
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-12-15 11:02:50 +0200

    added file utils support

---
+++ main/main.py
@@ -0,0 +1,16 @@
+import os
+
+from solution.kmp import kmp_search
+from utils.file_utils import read_from_file, write_to_file
+
+
+def main(filename):
+    os.chdir('../files/in')
+    input_data = read_from_file(filename)
+    string = input_data[0]
+    substring = input_data[1]
+    result = kmp_search(string, substring)
+    os.chdir('../out')
+    write_to_file(filename, result)
+    os.chdir('../')
+    return result
